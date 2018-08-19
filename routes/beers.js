931cbf9f2659adef54d17820ce5cf8c67ea0b9cf
Charlene Gipulan <charlenegipulan@MacBook-Pro.local>
2018-08-18 18:34:59 -0700

    add commment routing

--- routes/beers.js
+++ routes/beers.js
@@ -1,11 +1,12 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-var beersController = require('../controllers/beersController');
+var beersController = require("../controllers/beersController");
 
-router.get('/', beersController.index);
-router.get('/new', beersController.new);
-router.get('/:id', beersController.show);
-router.delete('/:id', beersController.delete);
-router.post('/', beersController.create);
+router.get("/", beersController.index);
+router.get("/new", beersController.new);
+router.get("/:id", beersController.show);
+router.delete("/:id", beersController.delete);
+router.post("/", beersController.create);
+router.post("/:id/comments", beersController.createComment);
 
 module.exports = router;
