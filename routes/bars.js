e0dfec229f31e2221a0fa3ff9594254b3b5bd913
Charlene Gipulan <charlenegipulan@MacBook-Pro.local>
2018-08-18 18:33:14 -0700

    add bar routes

--- routes/bars.js
+++ routes/bars.js
@@ -1,11 +1,14 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-var barsController = require('../controllers/barsController');
+var barsController = require("../controllers/bars");
 
-router.get('/', barsController.index);
-router.get('/new', barsController.new);
-router.get('/:id', barsController.show);
-router.post('/', barsController.create);
-router.delete('/:id', barsController.delete);
+router.get("/", barsController.index);
+router.get("/new", barsController.new);
+router.get("/:id", barsController.show);
+router.post("/", barsController.create);
+router.delete("/:id", barsController.delete);
+router.get("/:id/beers/new", barsController.newServe);
+router.post("/:barId/beers/:beerId", barsController.createServe);
+router.delete("/:barId/beers/:beerId", barsController.deleteServe);
 
 module.exports = router;
