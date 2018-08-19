59f9a7feab048cc98ab0eddf3aec73a29c943fb6
Charlene Gipulan <charlenegipulan@MacBook-Pro.local>
2018-08-18 18:27:03 -0700

    define beers controller modules

--- controllers/beers.js
+++ controllers/beers.js
@@ -1,34 +1,51 @@
-var Beer = require('../models/beer');
-var Bar = require('../models/bar');
-
-module.exports = {
-    index,
-    show,
-    new:  newBeer,
-    delete: deleteBeer,
-    create
-};
-
+var Beer = require("../models/beer");
 
 function index(req, res) {
-    Beer.find({}, (err, beers) => {
-      res.render('beers/index', {pageTitle: 'BEER LIST', beers});
+  Beer.find({}, (err, beers) => {
+    res.render("beers/index", { pageTitle: "BEER LIST", beers });
+  });
+}
+
+function show(req, res) {
+  Beer.findById(req.params.id)
+    .populate("bars")
+    .exec((err, beer) => {
+      res.render("beers/show", { pageTitle: "BEER: " + beer.name, beer });
     });
-  }
+}
 
-function show(res, req) {
-    res.render('beers/show');
-};
+function deleteBeer(req, res) {
+  Beer.findById(req.params.id, (err, beer) => {
+    beer.remove();
+    res.redirect("/beers");
+  });
+}
 
+function newBeer(req, res) {
+  res.render("beers/new", { pageTitle: "NEW BEER" });
+}
 
-function newBeer(res, req) {
-    res.render('beers/new');
-};
+function create(req, res) {
+  var beer = new Beer(req.body);
+  beer.save(err => {
+    res.redirect(`/beers/${beer.id}`);
+  });
+}
 
-function deleteBeer(res, req) {
-    res.redirect('beers/index');
-};
+function createComment(req, res) {
+  Beer.findById(req.params.id).exec((err, beer) => {
+    beer.comments.push({ content: req.body.content });
+    beer.save(err => {
+      res.redirect(`/beers/${beer.id}`);
+    });
+  });
+}
 
-function create(res, req) {
-    res.render('beers/create');
+module.exports = {
+  index,
+  show,
+  delete: deleteBeer,
+  new: newBeer,
+  create,
+  createComment
 };
